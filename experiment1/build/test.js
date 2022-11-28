73d8eeb21e2372bec9392d536c89a02e8816af7a
刘京宗 <470699397@qq.com>
2022-11-28 22:18:58 +0800

    finish experiment1

--- experiment1/build/test.js
+++ experiment1/build/test.js
@@ -1,130 +1,191 @@
-var datas = [];
-var keys = ["windSpeed", "moonPhase", "dewPoint", "humidity"
-  , "uvIndex", "windBearing", "temperatureMin", "temperatureMax"]
-var width = 800,
-  height = 400,
-  padding = {
-    top: 40,
-    right: 100,
-    bottom: 40,
-    left: 100
-  };
+async function updatingBars() {
+  // 1. Access data
+  const dataset = await d3.json('./data/weather_data.json')
 
-window.onload = async function () {
-  draw(0);
-  var i = 0;
-  //当id="btn"按钮被点击时,i增加
-  d3.select("#btn").on("click", function () {
-    //清空svg
-    d3.select("svg").remove();
-    //清空datas 
-    datas = [];
-    i++;
-    i %= keys.length;
-    draw(i);
-  })
-}
+  // 2. Create chart dimensions
 
-function draw(ii) {
-  var totaldata = d3.json("./data/weather_data.json").then(function (data) {
-    return data;
-  });
-  var i = ii;
-  var label = keys[i];
-  i++;
-  i %= keys.length;
-  label = keys[i];
-  totaldata.then(function (d) {
-    for (var i = 0; i < d.length; i++) {
-      datas.push(d[i][label]);
-    }
+  const width = 500
+  let dimensions = {
+    width: width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  }
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom
 
-    var svg = d3.select("#test-svg")
-      .append('svg')
-      .attr('width', width + 'px')
-      .attr('height', height + 'px');
+  // 3. Draw canvas
 
-    // x轴将数据映射从[min,max]到坐标轴
-    var xScale = d3.scaleLinear()
-      .domain([d3.min(datas), d3.max(datas) * 1.1])
-      .range([padding.left, width - padding.right]);
-    var xAxis = d3.axisBottom()
-      .scale(xScale)
-      .ticks(10);
-    svg.append('g')
-      .call(xAxis)
-      .attr("transform", "translate(0," + (height - padding.bottom) + ")");
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
 
-    // 构造一个直方图布局,在每个x轴刻度区域出现的次数
-    var his = d3.histogram()
+  const bounds = wrapper
+    .append('g')
+    .style(
+      'transform',
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    )
+
+  // init static elements
+  bounds.append('g').attr('class', 'bins')
+  bounds.append('line').attr('class', 'mean')
+  bounds
+    .append('g')
+    .attr('class', 'x-axis')
+    .style('transform', `translateY(${dimensions.boundedHeight}px)`)
+    .append('text')
+    .attr('class', 'x-axis-label')
+    .attr('x', dimensions.boundedWidth / 2)
+    .attr('y', dimensions.margin.bottom - 10)
+
+  const drawHistogram = metric => {
+    const metricAccessor = d => d[metric]
+    const yAccessor = d => d.length
+
+    const xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(dataset, metricAccessor))
+      .range([0, dimensions.boundedWidth])
+      .nice()
+
+    const binsGenerator = d3
+      .histogram()
       .domain(xScale.domain())
-      .thresholds(xScale.ticks(10))
-      (datas);
+      .value(metricAccessor)
+      .thresholds(12)
 
-    // y轴
-    var yScale = d3.scaleLinear()
-      .domain([0, d3.max(his,
-        function (d) {
-          return d.length;
-        })])
-      .range([height - padding.bottom, padding.top]);
-    var yAxis = d3.axisLeft()
-      .scale(yScale)
-      .ticks(10);
-    svg.append('g')
-      .call(yAxis)
-      .attr("transform", "translate(" + padding.left + ",0)");
+    const bins = binsGenerator(dataset)
 
-    var bar = svg.selectAll(".bar")
-      .data(his)
-      .join("g")
-      .attr("class", "bar")
-      .attr("transform",
-        function (d) {
-          return "translate(" + xScale(d.x0) + "," + yScale(d.length) + ")";
-        });
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(bins, yAccessor)])
+      .range([dimensions.boundedHeight, 0])
+      .nice()
+
+    // 5. Draw data
+    const barPadding = 1
+
+    const exitTransition = d3.transition().duration(600)
+    const updateTransition = exitTransition.transition().duration(600)
+
+    let binGroups = bounds.select('.bins').selectAll('.bin').data(bins)
+
+    const oldBinGroups = binGroups.exit()
+
+    // Select all of the rect elements to remove
+    oldBinGroups.selectAll("rect")
+      .style("fill", "red") // Color them red
+      .transition(exitTransition) // Use our new exit transition
+      // Shrink the bars into the x axis
+      .attr("y", dimensions.boundedHeight)
+      .attr("height", 0)
+
+    // Transition our text
+    oldBinGroups.selectAll("text")
+      .transition(exitTransition)
+      .attr("y", dimensions.boundedHeight)
+
+    // Actually remove our bars from the DOM once the transition has completed
+    oldBinGroups
+      .transition(exitTransition)
+      .remove()
+
+    const newBinGroups = binGroups.enter().append('g').attr('class', 'bin')
+
+    newBinGroups
+      .append('rect')
+      // IMPROVEMENT: Start in the right horizontal location and be 0 pixels tall to prevent bars from flying in from the left
+      .attr('height', 0)
+      .attr('x', d => xScale(d.x0) + barPadding)
+      .attr('y', dimensions.boundedHeight)
+      .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
+      // IMPROVEMENT: We need fill to be an inline style so that we can override it in our CSS file; otherwise this would just be an SVG attribute
+      .style('fill', 'yellowgreen')
+
+    newBinGroups.append('text')
+      // IMPROVEMENT: Set our labels' initial position to prevent them from flying in from the left
+      .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", dimensions.boundedHeight)
+
+    // update binGroups to include new points
+    binGroups = newBinGroups.merge(binGroups)
+
+    const barRects = binGroups
+      .select('rect')
+      // Transform our selection object into a D3 transition object
+      .transition(updateTransition) // IMPROVEMENT: Use our custom transition from above
+      .attr('x', d => xScale(d.x0) + barPadding)
+      .attr('y', d => yScale(yAccessor(d)))
+      .attr('height', d => dimensions.boundedHeight - yScale(yAccessor(d)))
+      .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
+      // Add a second transition to make sure our new bars are blue instead of the green color
+      .transition()
+      .style("fill", "cornflowerblue")
+
+    // make the rects change color when mouse over
+    binGroups.selectAll('rect').on('mouseover', onMouseEnter).on('mouseleave', onMouseLeave)
+    function onMouseEnter(d, i) {
+      d3.select(this).style('fill', 'orange')
+      d3.select("#tooltip")//位置在bar上方
+        .style("left", xScale(d.x0) + 210 + "px")
+        .style("top", yScale(yAccessor(d)) + 50 + "px")
+        .style("font-size", "12px")
+        .style("opacity", 1.0)
+        .html(d.length + " days");
+    }
+    function onMouseLeave() {
+      d3.select(this).style('fill', 'cornflowerblue')
+      d3.select("#tooltip")
+        .style("opacity", 0.0);
+    }
 
     var tooltip = d3.select("body")
       .append("div")
       .attr("id", "tooltip")
       .style("position", "absolute")
 
-    // 构造柱       
-    bar.append("rect")
-      .attr("x", 1)
-      .transition().duration(1000)
-      //设置柱的宽度与x轴刻度的宽度一致
-      .attr("width", function (d) { return (xScale(d.x1) - xScale(d.x0)) - 1 })
-      .attr("height",
-        function (d) {
-          return height - yScale(d.length) - padding.bottom;
-        })
-      .attr("fill", "steelblue");
 
-    //添加动画 鼠标触碰变颜色
-    d3.selectAll("rect")
-      .on("mouseover", function (d, i) {
-        d3.select(this).attr("fill", "red");
-        d3.select("#tooltip")//位置在bar上方
-          .style("left", xScale(d.x0) + 15 + "px")
-          .style("top", yScale(d.length) - 10 + "px")
-          .style("font-size", "12px")
-          .style("opacity", 1.0)
-          .html(d.length + " days");
-      })
-      .on("mouseout", function (d, i) {
-        d3.select(this).attr("fill", "steelblue");
-        d3.select("#tooltip")
-          .style("opacity", 0.0);
-      });
+    // 6. Draw peripherals
 
-    //svg底部添加label文本
-    svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", height - 5)
-      .attr("text-anchor", "middle")
-      .attr("font-size", "16px")
-      .text(label);
+    const xAxisGenerator = d3.axisBottom().scale(xScale)
 
-  });
+    const xAxis = bounds.select('.x-axis')
+      .transition(updateTransition) // IMPROVEMENT: Use our custom transition from above
+      //We can now see our tick marks move to fit the new domain before the new tick marks are drawn
+      .call(xAxisGenerator)
+
+    const xAxisLabel = xAxis.select('.x-axis-label').text(metric)
+  }
+
+  const metrics = [
+    'windSpeed',
+    'moonPhase',
+    'dewPoint',
+    'humidity',
+    'uvIndex',
+    'windBearing',
+    'temperatureMin',
+    'temperatureMax',
+  ]
+  let selectedMetricIndex = 0
+  drawHistogram(metrics[selectedMetricIndex])
+
+  const button = d3.select('body').append('button').text('Change metric')
+
+  button.node().addEventListener('click', onClick)
+
+  function onClick() {
+    selectedMetricIndex = (selectedMetricIndex + 1) % (metrics.length - 1)
+    drawHistogram(metrics[selectedMetricIndex])
+  }
 }
+updatingBars()
--- training/src/pages/Scatter2/index.tsx
+++ training/src/pages/Scatter2/index.tsx
@@ -9,23 +9,74 @@ const Scatter2: React.FC = () => {
   const { setS1Data } = useModel('test');
   const margin = { left: 30, right: 10, top: 10, bottom: 20 };
   const svg = d3.select(svgRef.current).select('.main');
-  const drawScatter = (data: any[]) => {
+  const drawScatter = (nodes: any[], links: any[]) => {
     const width = svgRef.current?.clientWidth!;
     const height = svgRef.current?.clientHeight!;
     const clipWidth = width - margin.left - margin.right;
     const clipheight = height - margin.top - margin.bottom;
     // 创建一个仿真器simulation
-    
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        'link',
+        d3.forceLink(links).id((d: any) => d.id),
+      )
+      .force('charge', d3.forceManyBody().strength(-100))
+      .force('center', d3.forceCenter(clipWidth / 2, clipheight / 2));
     // 添加边
-
+    const link = svg
+      .selectAll('.link')
+      .data(links)
+      .join('line')
+      .attr('class', 'link')
+      .attr('stroke', '#999')
+      .attr('stroke-opacity', 0.6)
+      .attr('stroke-width', (d: any) => Math.sqrt(d.value));
     // 添加点
-
+    const dragstarted = (event: any, d: any) => {
+      event.sourceEvent.stopPropagation();
+      if (!event.active) {
+        simulation.alphaTarget(0.3).restart();
+      }
+      d.fx = d.x;
+      d.fy = d.y;
+    };
+    const dragged = (event: any, d: any) => {
+      d.fx = event.x;
+      d.fy = event.y;
+      d.fixed = true;
+    };
+    const dragended = (event: any, d: any) => {
+      if (!event.active) {
+        simulation.alphaTarget(0);
+      }
+      d.fx = null;
+      d.fy = null;
+    };
+    const node = svg.append('g').attr("class", "nodegroup")
+      .selectAll('.node')
+      .data(nodes)
+      .join('circle')
+      .attr('class', 'node')
+      .attr('r', 5)
+      .attr('fill', '#fff')
+      .attr('stroke', '#000')
+      .attr('stroke-width', 1.5)
+      .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended) as any);
+    
     simulation.on('tick', ticked);
     // 为仿真器添加ticked函数
     function ticked() {
-
-
-
+      link
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y)
+      node
+        .attr('transform', (d: { x: any; y: any; }) => `translate(${d.x},${d.y})`)
     }
     // 定义矩形选择框
     const brush = d3
@@ -46,7 +97,7 @@ const Scatter2: React.FC = () => {
   };
 
   useEffect(() => {
-    drawScatter(data);
+    drawScatter(data.nodes, data.links);
   }, []);
 
   return (
